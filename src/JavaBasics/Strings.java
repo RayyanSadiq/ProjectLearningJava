bd924ce7d04f0d4543f6a630a9223f379e96cfba
DragonXdroid <raysadik5@gmail.com>
2022-10-24 08:59:24 -0500

    commit 7.3

--- src/JavaBasics/Strings.java
+++ src/JavaBasics/Strings.java
@@ -15,18 +15,42 @@ public class Strings {
         System.out.println(message.replace("ll","gg")); // prints "Heggo world"
         System.out.println(message.toLowerCase());
         System.out.println(message.toUpperCase());
+        System.out.println("\n");
 
-        //Escape sequences
+        //-----------------------------------------------Escape sequences---------------------------------------------\\
         System.out.println("look its a backslash = \\");
-        System.out.println("with back slash we can go print quotes and double quotes in strings,\'\"\nwe can also go to the next line ");
+        System.out.println("with back slash we can go print quotes and double quotes in strings,\'\"\nwe can also go to the next line");
+        System.out.println("\n");
+        //--------------------------------------------The Substring method--------------------------------------------\\
+        System.out.println(message.substring(0,3).toUpperCase()); // String methods can be combined like this
+        /* the way substring work is that the first parameter takes
+        in the beginning index to print and the second parameter takes the range to print up to. The letter at the third index
+        however will not be in the String, only up till the third letter, but not including the third letter.*/
 
-        System.out.println(message.substring(0,3).toUpperCase());
+        String halfString = message.substring(2);
+        System.out.println(halfString);
+        // Another thing to keep in mind is that when only the beginning index is given to the substring method, then it will
+        // print all elements from the beginning index till the end.
+
         // Like many other languages, when indexing iterables, the index position ranges from [0-infinty], it starts
         // from 0, not 1.
 
         System.out.println(message.substring(0,2) + message.substring(message.length() -2));
         int position = message.indexOf("orl");
         System.out.println(message.substring(position));
+
+        System.out.println("\n"); // this line is purely for spacing aesthetic
+
+
+        //------------------------------------------Compare to method-------------------------------------------------\\
+
+        String string1  = "hello"; // learn compare to and the API of i
+        String string2 = "hello";
+        String string3 = "me";
+
+        System.out.println(string1.compareTo(string2));
+
+
 
 
 
--- src/OOP/Inheritance_And_Polymorphism/Part1/MainPoint.java
+++ src/OOP/Inheritance_And_Polymorphism/Part1/MainPoint.java
@@ -5,8 +5,11 @@ public class MainPoint {
         var point1 = new PointClass(1,2);
         var point2 = new PointClass(1,2);
         System.out.println(point2.equals( point1)); // over here we are checking out our new overridden equals method
-        System.out.println(point2.hashCode()); // here we are checking our overridden hashcode method.
-        System.out.println(point1.hashCode()); // since this object has the same coordinates as the other one, they both
+        System.out.println(point1.hashCode());
+        // the hashcode affects the address too, if two hashes are the same, then the address would also be the same of those 2 objects.
+        // so when we override the hashcode() function we are basing the hashcode on the contents of the class,
+        System.out.println(point2); // here we are checking our overridden hashcode method.
+        System.out.println(point1); // since this object has the same coordinates as the other one, they both
         // have the same hash code
     }
 }
